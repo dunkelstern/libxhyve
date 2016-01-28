13c40cb2dd3685a57e03392e5616e6be13745de2
Johannes Schriewer <hallo@dunkelstern.de>
2016-01-28 01:48:34 +0100

    Add 'InitDisk' function to go bindings to create a virtual disk without running the VM

--- xhyve.go
+++ xhyve.go
@@ -5,6 +5,7 @@ package xhyve
 // #cgo CFLAGS: -I${SRCDIR}/include -x c -std=c11 -fno-common -arch x86_64 -DXHYVE_CONFIG_ASSERT -DVERSION=v0.2.0 -Os -fstrict-aliasing -Wno-unknown-warning-option -Wno-reserved-id-macro -pedantic -fmessage-length=152 -fdiagnostics-show-note-include-stack -fmacro-backtrace-limit=0
 // #cgo LDFLAGS: -L${SRCDIR} -arch x86_64 -framework Hypervisor -framework vmnet
 // #include <xhyve/xhyve.h>
+// #include <xhyve/block_if.h>
 // #include <xhyve/mevent.h>
 // #include <string.h>
 import "C"
@@ -117,4 +118,9 @@ func Run(params []string, newPtyCh chan string) error {
 	}
 
 	return nil
+}
+
+func InitDisk(config string) {
+	ctx := C.blockif_open(C.CString(config), nil)
+	C.blockif_close(ctx)
 }
